20ac6553fd2a83f8da13cc2e794a46f6615e57a0
RayannaLiene <lienelorenconderezenderayanna@gmail.com>
2023-04-20 08:55:37 -0300

    aula do dia 20-04

--- Src/controllers.js
+++ Src/controllers.js
@@ -34,18 +34,73 @@ module.exports = {
               })
         }
 
-        products.push({
+        const existsDescription = products.find(product => product.descricao.toLocaleLowerCase())
+        if (existsDescription) {
+            return response.status(400).json({
+                error: 'Produto já cadastrado'
+            })
+        }
+
+        const product = {
             id,
             descricao,
             valor,
             qtde
-        
-        })
+        }
 
+        products.push(product)
+        
         return response.json({data: products})
     },
 
-    async hello2(request, response) {
-        return response.json({ msg: 'hello world'})
+    async getListOfCart(request, response) {
+        return response.json({ data: products})
+    },
+
+    async createUserCart(request, response) {
+
+        const { item } = request.body
+
+
+        for (const product of product) {
+            const productExists = products.find(prd => prd.id === product.id)
+           
+            if (!productExists) {
+               return response.status(400).json({
+                error: 'Produto não encontrado'
+               })
+            }
+        }
+
+        if (product.qtde > productsExists.qtde) {
+            return response.status(400).json({
+                error: 'Quantidade não disponivel'
+            })
+        }
+
+        const useritens = {
+            productId: productExists.id,
+            descricao: productExists.descricao,
+            qtde: product.qtde,
+            valor: product.qtde * productExists.valor
+        }
+
+       userCart.push(userItens) 
+
+       const index = products.findIndex(idx => idx.id === product.id)
+       products[index].qtde = products[index].qtde - product.qtde
+       //products[index].qtde -= product.qtde -> decremento
+       //products[index].qtde += product.qtde -> incremento
+    
+       return response.json({
+        order: userCart,
+        stock: products
+       })
+
+
+
+
+
+        return response.json({ data: true})
     }
 }
